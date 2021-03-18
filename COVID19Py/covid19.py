10bfeb0afb9d70bfc24e95adad180af3cb641988
Nicholas Logan <logan206@my.yorku.ca>
2021-03-17 20:05:28 -0400

    Aggregate example 2: Data class added, usage demo added in sample main()

--- COVID19Py/covid19.py
+++ COVID19Py/covid19.py
@@ -3,85 +3,40 @@ import requests
 import json
 
 class COVID19(object):
-    default_url = "https://covid-tracker-us.herokuapp.com"
-    url = ""
-    data_source = ""
-    previousData = None
-    latestData = None
-    _valid_data_sources = []
+    data = None
 
-    mirrors_source = "https://raw.github.com/Kamaropoulos/COVID19Py/master/mirrors.json"
-    mirrors = None
-
-    def __init__(self, url="https://covid-tracker-us.herokuapp.com", data_source="jhu"):
-        # Skip mirror checking if custom url was passed
-        if url == self.default_url:
-            # Load mirrors
-            response = requests.get(self.mirrors_source)
-            response.raise_for_status()
-            self.mirrors = response.json()
-
-            # Try to get sources as a test
-            for mirror in self.mirrors:
-                # Set URL of mirror
-                self.url = mirror["url"]
-                result = None
-                try:
-                    result = self._getSources()
-                except Exception as e:
-                    # URL did not work, reset it and move on
-                    self.url = ""
-                    continue
-
-                # TODO: Should have a better health-check, this is way too hacky...
-                if "jhu" in result:
-                    # We found a mirror that worked just fine, let's stick with it
-                    break
-
-                # None of the mirrors worked. Raise an error to inform the user.
-                raise RuntimeError("No available API mirror was found.")
-
-        else:
-            self.url = url
-
-        self._valid_data_sources = self._getSources()
-        if data_source not in self._valid_data_sources:
-            raise ValueError("Invalid data source. Expected one of: %s" % self._valid_data_sources)
-        self.data_source = data_source
+    def __init__(self, data=None):
+        if data is not None:
+            self.data = data
 
     def _update(self, timelines):
         latest = self.getLatest()
         locations = self.getAllLocations(timelines)
-        if self.latestData:
-            self.previousData = self.latestData
-        self.latestData = {
+        if self.data.latestData:
+            self.data.previousData = self.data.latestData
+        self.data.latestData = {
             "latest": latest,
             "locations": locations
         }
 
-    def _getSources(self):
-        response = requests.get(self.url + "/v2/sources")
-        response.raise_for_status()
-        return response.json()["sources"]
-
     def _request(self, endpoint, params=None):
         if params is None:
             params = {}
-        response = requests.get(self.url + endpoint, {**params, "source":self.data_source})
+        response = requests.get(self.data.url + endpoint, {**params, "source":self.data.data_source})
         response.raise_for_status()
         return response.json()
 
     def getAll(self, timelines=False):
         self._update(timelines)
-        return self.latestData
+        return self.data.latestData
 
     def getLatestChanges(self):
         changes = None
-        if self.previousData:
+        if self.data.previousData:
             changes = {
-                "confirmed": self.latestData["latest"]["confirmed"] - self.latestData["latest"]["confirmed"],
-                "deaths": self.latestData["latest"]["deaths"] - self.latestData["latest"]["deaths"],
-                "recovered": self.latestData["latest"]["recovered"] - self.latestData["latest"]["recovered"],
+                "confirmed": self.data.latestData["latest"]["confirmed"] - self.data.latestData["latest"]["confirmed"],
+                "deaths": self.data.latestData["latest"]["deaths"] - self.data.latestData["latest"]["deaths"],
+                "recovered": self.data.latestData["latest"]["recovered"] - self.data.latestData["latest"]["recovered"],
             }
         else:
             changes = {
@@ -156,6 +111,60 @@ class COVID19(object):
             return data["location"]
 
 
+class Data(object):
+    default_url = "https://covid-tracker-us.herokuapp.com"
+    url = ""
+    data_source = ""
+    previousData = None
+    latestData = None
+    _valid_data_sources = []
+
+    mirrors_source = "https://raw.github.com/Kamaropoulos/COVID19Py/master/mirrors.json"
+    mirrors = None
+
+    def __init__(self, url="https://covid-tracker-us.herokuapp.com", data_source="jhu"):
+        # Skip mirror checking if custom url was passed
+        if url == self.default_url:
+            # Load mirrors
+            response = requests.get(self.mirrors_source)
+            response.raise_for_status()
+            self.mirrors = response.json()
+
+            # Try to get sources as a test
+            for mirror in self.mirrors:
+                # Set URL of mirror
+                self.url = mirror["url"]
+                result = None
+                try:
+                    result = self._getSources()
+                except Exception as e:
+                    # URL did not work, reset it and move on
+                    self.url = ""
+                    continue
+
+                # TODO: Should have a better health-check, this is way too hacky...
+                if "jhu" in result:
+                    # We found a mirror that worked just fine, let's stick with it
+                    break
+
+                # None of the mirrors worked. Raise an error to inform the user.
+                raise RuntimeError("No available API mirror was found.")
+
+        else:
+            self.url = url
+
+        self._valid_data_sources = self._getSources()
+        if data_source not in self._valid_data_sources:
+            raise ValueError("Invalid data source. Expected one of: %s" % self._valid_data_sources)
+        self.data_source = data_source
+
+    def _getSources(self):
+        response = requests.get(self.url + "/v2/sources")
+        response.raise_for_status()
+        return response.json()["sources"]
+
+
+
 class Location(object):
     country = ""
     country_code = ""
@@ -178,7 +187,7 @@ class Location(object):
 
 # Testing
 # def main():
-#     tracker = COVID19(data_source="jhu")
+#     tracker = COVID19(Data())
 #     print(tracker.getLocationData(Location(country_code="CA")))
 
 # if __name__ == '__main__':
