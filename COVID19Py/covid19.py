eeb6f8a465f0c1542ed842ffc23c3b381bb2c590
nicholas karafilov <nicholas.karafilov9@gmail.com>
2021-04-15 19:41:11 -0400

    Singleton Pattern Applied

--- COVID19Py/covid19.py
+++ COVID19Py/covid19.py
@@ -2,19 +2,26 @@ from typing import Dict, List
 import requests
 import json
 
-class COVID19(object):
+class COVIDSingleton(object):
+
+    
     default_url = "https://covid-tracker-us.herokuapp.com"
     url = ""
     data_source = ""
     previousData = None
     latestData = None
+    __instance = None
     _valid_data_sources = []
 
     mirrors_source = "https://raw.github.com/Kamaropoulos/COVID19Py/master/mirrors.json"
     mirrors = None
 
     def __init__(self, url="https://covid-tracker-us.herokuapp.com", data_source='jhu'):
-        # Skip mirror checking if custom url was passed
+        
+        if Singleton.__instance is None:
+                Singleton.__instance = object.__new__(self)
+                     
+       # Skip mirror checking if custom url was passed 
         if url == self.default_url:
             # Load mirrors
             response = requests.get(self.mirrors_source)
@@ -49,6 +56,13 @@ class COVID19(object):
             raise ValueError("Invalid data source. Expected one of: %s" % self._valid_data_sources)
         self.data_source = data_source
 
+    def SingletonInstance():
+        if COVIDSingleton.__instance is not None:
+            return COVIDSingleton._instance
+        else:
+            COVIDSingleton()
+            return COVIDSingleton._intance
+        
     def _update(self, timelines):
         latest = self.getLatest()
         locations = self.getLocations(timelines)
