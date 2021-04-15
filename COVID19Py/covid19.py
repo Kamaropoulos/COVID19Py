832d6c2954c7ba407ed6deef8a72e91191fcc01c
BLI <zyclone271@outlook.com>
2021-04-15 00:59:40 -0400

    Applied Creational Design Pattern

--- COVID19Py/covid19.py
+++ COVID19Py/covid19.py
@@ -2,17 +2,30 @@ from typing import Dict, List
 import requests
 import json
 
-#class acting as an aggregate for all operations related to setting up the user's data retrieval request
-class UserRequestSetup(object):
-    __default_url = "https://covid-tracker-us.herokuapp.com"
+class COVID19(object):
+    # 
+    default_url = "https://covid-tracker-us.herokuapp.com"
     url = ""
     data_source = ""
+    previousData = None
+    latestData = None
     _valid_data_sources = []
 
-    __mirrors_source = "https://raw.github.com/Kamaropoulos/COVID19Py/master/mirrors.json"
+    mirrors_source = "https://raw.github.com/Kamaropoulos/COVID19Py/master/mirrors.json"
     mirrors = None
 
+    # Declaration of single private static instance of the class. Done to fulfill requirement for the Singleton Creational Design Pattern.
+    __singlePrivateInstance = None
+
+    # Constructor. Already privated, meaning that its already fulfills a requirement for the Singleton Creational Design Pattern.
     def __init__(self, url="https://covid-tracker-us.herokuapp.com", data_source='jhu'):
+
+	# Checks to see if an instance has already been created. If it has, we put out an error message. Otherwise, we create a new instance.
+        if COVID19.__singlePrivateInstance != None:
+	    raise Exception("Singleton pattern has been applied. An instance was previously created, hence new instance cannot be created. Use getCOVID19Instance() to access the instance.")
+        else:
+            COVID19.__singlePrivateInstance = self
+
         # Skip mirror checking if custom url was passed
         if url == self.default_url:
             # Load mirrors
@@ -49,35 +62,15 @@ class UserRequestSetup(object):
         self.data_source = data_source
 
 
-    def _getSources(self):
-        response = requests.get(self.url + "/v2/sources")
-        response.raise_for_status()
-        return response.json()["sources"]
+    # Public method that allows access to the single instance. Requirement for Singleton Creation Design Pattern.
+    @staticmethod
+    def getCOVID19Instance():
+        if COVID19.__singlePrivateInstance == None
+            COVID19()
+        return COVID19.__singlePrivateInstance
 
 
-    #originally _request, underscore removed as it is now referred to by class COVID19DataRetrieval and requires access. Underscore indicates protected status
-    def request(self, endpoint, params=None):
-        if params is None:
-            params = {}
-        response = requests.get(self.url + endpoint, {*params, "source": self.data_source}) #fixed a syntax error (originally **)
-        response.raise_for_status()
-        return response.json()
-   
- 
-
-#class acting as an aggregate for generalized data retrieval operations
-class COVID19GeneralizedDataRetrieval(object):
-    #originally variables from the class COVID19. Variables are only used in the operations contained within the COVIDDataRetrieval class, hence they were moved here
-    __previousData = None
-    __latestData = None
-
-
-    #"constructor" method that initializes data members of the class, where applicable
-    def __init__(self, userrequestsetup)
-	self.userrequestsetup = userrequestsetup #enables/allows us to access methods in the UserRequestSetup class, mainly the request method
-
-
-    #updates to latest data
+    # Updates to latest data
     def _update(self, timelines):
         latest = self.getLatest()
         locations = self.getLocations(timelines)
@@ -88,33 +81,36 @@ class COVID19GeneralizedDataRetrieval(object):
             "locations": locations
         }
 
-#############################
-# Generalized data searches
-#############################
 
-    #get all data at once
+    # Gets data sources
+    def _getSources(self):
+        response = requests.get(self.url + "/v2/sources")
+        response.raise_for_status()
+        return response.json()["sources"]
+
+
+    def _request(self, endpoint, params=None):
+        if params is None:
+            params = {}
+        response = requests.get(self.url + endpoint, {**params, "source": self.data_source})
+        response.raise_for_status()
+        return response.json()
+
+
+    # Gets all data at once
     def getAll(self, timelines=False):
         self._update(timelines)
         return self.latestData
 
 
-    #gets the latest COVID-19 case data
-    def getLatest(self) -> List[Dict[str, int]]:
-        """
-        :return: The latest amount of total confirmed cases, deaths, and recoveries.
-        """
-        data = self.userrequestsetup.request("/v2/latest")
-        return data["latest"]
-
-
-    #get the latest changes (i.e.latest deltas)
+    # Gets the latest changes in COVID-19 case data since last request
     def getLatestChanges(self):
         changes = None
         if self.previousData:
             changes = {
-                "confirmed": self.latestData["latest"]["confirmed"] - self.previousData["latest"]["confirmed"], #changed from latestData to previousData
-                "deaths": self.latestData["latest"]["deaths"] - self.previousData["latest"]["deaths"],		#changed from latestData to previousData
-                "recovered": self.latestData["latest"]["recovered"] - self.previousData["latest"]["recovered"], #changed from latestData to previousData
+                "confirmed": self.latestData["latest"]["confirmed"] - self.previousData["latest"]["confirmed"],
+                "deaths": self.latestData["latest"]["deaths"] - self.previousData["latest"]["deaths"],
+                "recovered": self.latestData["latest"]["recovered"] - self.previousData["latest"]["recovered"],
             }
         else:
             changes = {
@@ -125,7 +121,16 @@ class COVID19GeneralizedDataRetrieval(object):
         return changes
 
 
-    #get case data relating all locations with COVID-19 
+    # Gets the latest COVID-19 case data
+    def getLatest(self) -> List[Dict[str, int]]:
+        """
+        :return: The latest amount of total confirmed cases, deaths, and recoveries.
+        """
+        data = self._request("/v2/latest")
+        return data["latest"]
+
+
+    # Get COVID-19 case data relating to all locations with COVID-19
     def getLocations(self, timelines=False, rank_by: str = None) -> List[Dict]:
         """
         Gets all locations affected by COVID-19, as well as latest case data.
@@ -135,11 +140,12 @@ class COVID19GeneralizedDataRetrieval(object):
         """
         data = None
         if timelines:
-            data = self.userrequestsetup.request("/v2/locations", {"timelines": str(timelines).lower()})
+            data = self._request("/v2/locations", {"timelines": str(timelines).lower()})
         else:
-            data = self.userrequestsetup.request("/v2/locations")
+            data = self._request("/v2/locations")
 
         data = data["locations"]
+        
         ranking_criteria = ['confirmed', 'deaths', 'recovered']
         if rank_by is not None:
             if rank_by not in ranking_criteria:
@@ -147,36 +153,11 @@ class COVID19GeneralizedDataRetrieval(object):
 
             ranked = sorted(data, key=lambda i: i['latest'][rank_by], reverse=True)
             data = ranked
+
         return data
 
 
-#class acting as an aggregate for country-based data retrieval operations
-class COVID19CountryBasedDataRetrieval(object):
-
-    #"constructor" method that initializes data members of the class, where applicable
-    def __init__(self, userrequestsetup)
-	self.userrequestsetup = userrequestsetup #enables/allows us to access methods in the UserRequestSetup class, mainly the request method
-
-###############################
-# Country-based data searches
-###############################
-
-    #get case data by method of country name
-    def getLocationByCountry(self, country, timelines=False) -> List[Dict]:
-        """
-        :param country: String denoting name of the country
-        :param timelines: Whether timeline information should be returned as well.
-        :return: A list of areas that correspond to the country name. If the country is invalid, it returns an empty list.
-        """
-        data = None
-        if timelines:
-            data = self.userrequestsetup.request("/v2/locations", {"country": country, "timelines": str(timelines).lower()})
-        else:
-            data = self.userrequestsetup.request("/v2/locations", {"country": country})
-        return data["locations"]
-
-
-    #get case data by method of country code
+    # Get COVID-19 case data by method of country code
     def getLocationByCountryCode(self, country_code, timelines=False) -> List[Dict]:
         """
         :param country_code: String denoting the ISO 3166-1 alpha-2 code (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) of the country
@@ -185,17 +166,32 @@ class COVID19CountryBasedDataRetrieval(object):
         """
         data = None
         if timelines:
-            data = self.userrequestsetup.request("/v2/locations", {"country_code": country_code, "timelines": str(timelines).lower()})
+            data = self._request("/v2/locations", {"country_code": country_code, "timelines": str(timelines).lower()})
         else:
-            data = self.userrequestsetup.request("/v2/locations", {"country_code": country_code})
+            data = self._request("/v2/locations", {"country_code": country_code})
+        return data["locations"]
+    
+
+    # Get COVID-19 case data by method of country name
+    def getLocationByCountry(self, country, timelines=False) -> List[Dict]:
+        """
+        :param country: String denoting name of the country
+        :param timelines: Whether timeline information should be returned as well.
+        :return: A list of areas that correspond to the country name. If the country is invalid, it returns an empty list.
+        """
+        data = None
+        if timelines:
+            data = self._request("/v2/locations", {"country": country, "timelines": str(timelines).lower()})
+        else:
+            data = self._request("/v2/locations", {"country": country})
         return data["locations"]
 
 
-    #get case data by method of country id
+    # Get COVID-19 case data by method of country id
     def getLocationById(self, country_id: int):
         """
         :param country_id: Country Id, an int
         :return: A dictionary with case information for the specified location.
         """
-        data = self.userrequestsetup.request("/v2/locations/" + str(country_id))
+        data = self._request("/v2/locations/" + str(country_id))
         return data["location"]
