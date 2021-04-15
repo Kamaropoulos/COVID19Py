766c46ef71d4442a695e7d625523dfb02d83e178
Dan Rechtman <dannyrechtman@gmail.com>
2021-04-15 17:03:52 -0400

    Added Covid19Implementation and its interface

---
+++ COVID19Py/implementation.py
@@ -0,0 +1,29 @@
+from abc import ABC,abstractmethod
+from typing import Dict, List
+
+
+class Implementation(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def getEndLatest(self):
+        pass
+    
+    @staticmethod
+    @abstractmethod
+    def getEndLocations(self):
+        pass
+    
+class COVID19Implementation(Implementation):
+    
+    def __init__(self,url,data_source="jhu"):
+        self.url = url
+        self.data_source = data_source
+
+    @staticmethod
+    def getEndLatest(self):
+        return "/v2/latest"
+
+     @staticmethod
+    def getEndLocations(self):
+        return "/v2/locations"
